689dda445c60e889a504c934cd1f18d41abb89a8
Tang Xiadi <tangxiadi@gmail.com>
2015-01-30 03:09:32 +0800

    Update

--- Update.bat
+++ Update.bat
@@ -1,11 +1,12 @@
 @echo off
-git status >%tmp%\status.tmp
+git status>%tmp%\status.tmp
 for /F "eol=  tokens=1,2,3 delims=	/" %%i in (%tmp%\status.tmp) do (
 if %%i == images (
-echo http://tangxiadi.github.io/images/%%j
-
+echo http://tangxiadi.github.io/images/%%j>%tmp%\clip.tmp
+clip<%tmp%\clip.tmp
 )
 )
+del %tmp%\clip.tmp
 del %tmp%\status.tmp
 Pause
 git add .
