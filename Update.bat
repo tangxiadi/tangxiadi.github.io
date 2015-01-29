5a80cc81f1a558ad0a0f8b45ef3700e5244b1f35
Tang Xiadi <tangxiadi@gmail.com>
2015-01-30 02:54:42 +0800

    Update

--- Update.bat
+++ Update.bat
@@ -1,4 +1,9 @@
 @echo off
+git status >status.tmp
+FOR /F "eol=  tokens=1,2,3 delims=	/" %%i in (status.tmp) do echo %%i %%j %%k
+Pause
+Del test.txt
+
 git add .
 git commit -a -m "Update"
 git push
