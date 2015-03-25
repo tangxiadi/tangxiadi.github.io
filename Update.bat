ffb2265e0b0dda98274e23c3430386709adc7cdd
Tang Xiadi <tangxiadi@gmail.com>
2015-03-26 04:54:36 +0800

    Update

--- Update.bat
+++ Update.bat
@@ -1,16 +1,5 @@
 @echo off
-git status>%tmp%\status.tmp
-for /F "eol=  tokens=1,2,3 delims=	/" %%i in (%tmp%\status.tmp) do (
-if %%i == images (
-echo http://tangxiadi.github.io/images/%%j>%tmp%\clip.tmp
-clip<%tmp%\clip.tmp
-)
-)
-del %tmp%\clip.tmp 2>nul
-del %tmp%\status.tmp 2>nul
+for /F "eol=  tokens=1,2,3 delims=	/" %%i in ('git status') do if %%i == images echo http://tangxiadi.github.io/images/%%j|clip
 git add .
 git commit -a -m "Update"
 git push
-exit
-echo.
-cls & echo Push complete, image URL has been copied to the clipboard! & pause 1>nul
