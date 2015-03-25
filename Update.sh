dd1712fc82992b1320f211e270e928cd12358e75
tangxiadi <tangxiadi@gmail.com>
2015-03-25 21:55:01 +0800

    Update

---
+++ Update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#git status>%tmp%\status.tmp
+#for /F "eol=  tokens=1,2,3 delims=	/" %%i in (%tmp%\status.tmp) do (
+#if %%i == images (
+#echo http://tangxiadi.github.io/images/%%j>%tmp%\clip.tmp
+#clip<%tmp%\clip.tmp
+#)
+#)
+#del %tmp%\clip.tmp 2>nul
+#del %tmp%\status.tmp 2>nul
+git add .
+git commit -a -m "Update"
+git push
+read -p "Press any key to continue." var
