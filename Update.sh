ee3f942f505c965d8fef6982c20b586ee6d5df87
tangxiadi <tangxiadi@gmail.com>
2015-03-31 21:14:39 +0800

    Update

--- Update.sh
+++ Update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-str=`git status|grep "images"`
-echo "http://tangxiadi.github.io/"${str:1}|xsel -b
+if
+str=`git status|grep "images"`|xsel -b
+then
+echo "http://tangxiadi.github.io/"${str:1}
+fi
 git add .
 git commit -a -m "Update"
 git push
