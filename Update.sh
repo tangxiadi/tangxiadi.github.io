a61b4f3ab187e01d487d115c00039e024d46836f
tangxiadi <tangxiadi@gmail.com>
2015-03-31 21:15:42 +0800

    Update

--- Update.sh
+++ Update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if
-str=`git status|grep "images"`|xsel -b
+str=`git status|grep "images"`
 then
-echo "http://tangxiadi.github.io/"${str:1}
+echo "http://tangxiadi.github.io/"${str:1}|xsel -b
 fi
 git add .
 git commit -a -m "Update"
 git push
-#read -p "Press any key to continue." var
+read -p "Press any key to continue." var
