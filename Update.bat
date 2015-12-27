f2795ddb4f6170e44447aa06a2f0e53da37f9e08
tangxiadi <tangxiadi@gmail.com>
2015-12-27 11:59:23 +0800

    Update

--- Update.bat
+++ Update.bat
@@ -1,4 +1,5 @@
 @echo off
+set PATH=%PATH%;"D:\Program Files (x86)\Git\bin"
 for /F "eol=  tokens=1,2,3 delims=	/" %%i in ('git status') do if %%i == images echo http://tangxiadi.github.io/images/%%j|clip
 git add .
 git commit -a -m "Update"
