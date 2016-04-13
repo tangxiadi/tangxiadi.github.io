ed991c3f8b376acbaf61dbb07af6a6eff7a7a53f
Rudy <Rudy@RudydeMacBook-Air.local>
2016-04-14 02:37:15 +0800

    Update

---
+++ Update_OSX.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if
+str=`git status|grep "	images"`
+then
+echo "http://tangxiadi.github.io/"${str:1}|pbcopy
+fi
+git add .
+git commit -a -m "Update"
+git push
+#read -p "Press any key to continue." var
