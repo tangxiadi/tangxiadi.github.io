176be3cb1f934c5377daa767c76becaf70dcebac
Rudy <Rudy@RudydeMacBook-Air.local>
2016-04-14 02:57:15 +0800

    Update

--- Update_Windows.bat
+++ Update_Windows.bat
@@ -1,5 +1,7 @@
 @echo off
-for /F "eol=  tokens=1,2,3 delims=	/" %%i in ('git status') do if %%i == images echo http://tangxiadi.github.io/images/%%j|clip
+for /F "eol=  tokens=1,2,3 delims=	/" %%i in ('git status') do (
+if %%i == images echo http://tangxiadi.github.io/images/%%j|clip
+)
 git add .
 git commit -a -m "Update"
 git push
